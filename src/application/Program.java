8f29f7fde11c9f466b8956a51199aa49ad84076b
Rafael Pardim <ppardim27@gmail.com>
2024-08-08 09:00:15 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"Book");
+		
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
 		System.err.println(obj);
+		System.err.println(seller);
 
 	}
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -2,7 +2,6 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
